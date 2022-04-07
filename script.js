a3589aa7d537db56d05da323d4a3483c0cb3479c
aisurumari <80290357+aisurumari@users.noreply.github.com>
2022-04-07 17:03:09 +0200

    All the files for the final project

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    let title = document.getElementById("title").value;
+    let lname = document.getElementById("lname").value;
+    alert(`Thanks ${title} ${lname}, your registration is done!`);
+  }
+
+  //Maria Magdalena WICHROWSKA
+  //the file was changed to .txt because otherwise I couldn't send it by email
